1c5fa42088680eb9ae30fc437d5413364f38e972
bmS0621 <149003393+bmS0621@users.noreply.github.com>
2024-01-22 00:12:09 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,27 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     new window.TypeIt("#aluguel-passeio", {
+//       lifeLike: false,
+//       speed: 100,
+//       loop: true,
+//     //   cursorChar: " "
+//     })
+//       .type("Aluguel", { delay: 3550 })
+//       .delete()
+//       .type("Passeio", { delay: 3550 })
+//       .delete()
+//       .go();
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     new window.TypeIt("#jet-bote", {
+//       lifeLike: false,
+//       speed: 100,
+//       loop: true,
+//       cursorChar: " "
+//     })
+//       .type("Jet-Ski", { delay: 1500 })
+//       .delete()
+//       .type("Bote", { delay: 1500 })
+//       .delete()
+//       .go();
+//   });
